24f2328cc1556bd485eb5091d2de387dbea5fe64
Robson Renan da Silva <101532954+reenan71@users.noreply.github.com>
2023-01-03 22:48:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,16 @@ function lampOff () {
     lamp.src = "assets/lampada-desligada.jpg"
 }
 
+function lampBroken(){
+    lamp. src = "assets/lampada-quebrada.jpg"
+}
+
 
 
 
 turnOn.addEventListener ( 'click' , lampOn);
 turnOff.addEventListener ( 'click' , lampOff);
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
 
